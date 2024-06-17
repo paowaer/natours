752b9f9482055f2aa51b0b6331620b168d67eef7
Ian OU <oyjf012@gmail.com>
2024-06-16 18:43:21 -0700

    learning MERN

---
+++ server.js
@@ -0,0 +1,12 @@
+const dotenv = require('dotenv');
+
+const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+
+console.log(process.env);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App is running on port ${port}...`);
+});
